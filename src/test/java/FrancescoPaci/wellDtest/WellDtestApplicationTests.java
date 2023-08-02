23a2287a2d4cbe92a1610fe19516465e6565642d
fpaci <f.paci82@gmail.com>
2023-08-02 22:33:13 +0200

    aggiunto test di getLines

--- src/test/java/FrancescoPaci/wellDtest/WellDtestApplicationTests.java
+++ src/test/java/FrancescoPaci/wellDtest/WellDtestApplicationTests.java
@@ -1,13 +1,24 @@
 package FrancescoPaci.wellDtest;
 
+import FrancescoPaci.wellDtest.controller.LineController;
+import FrancescoPaci.wellDtest.model.Line;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
 
 @SpringBootTest
 class WellDtestApplicationTests {
 
+	@Autowired
+	LineController lineController;
+
 	@Test
-	void contextLoads() {
+	void getLines() {
+		List<Line> lines =  lineController.getLines();
+		Assert.isTrue(lines.size() > 0, "OK");
 	}
 
 }
